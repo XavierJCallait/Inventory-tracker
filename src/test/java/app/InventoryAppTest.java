cddd3d3c6d59a1ef05ac9d844511a04399218551
Xavier Callait <xaviercallait@gmail.com>
2025-07-31 23:38:49 -0700

    Added POM file for Maven and updated gitignore

---
+++ src/test/java/app/InventoryAppTest.java
@@ -0,0 +1,18 @@
+package app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class InventoryAppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
