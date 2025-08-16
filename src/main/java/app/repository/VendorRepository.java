79f7ecff70db2cf34a4049b01dfcc23a4b6e2d9b
Xavier Callait <xaviercallait@gmail.com>
2025-08-15 17:02:48 -0700

    Fixed line breaking my unit tests

--- src/main/java/app/repository/VendorRepository.java
+++ src/main/java/app/repository/VendorRepository.java
@@ -1,12 +1,12 @@
 package app.repository;
 
-import app.model.Vendor;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import app.model.Vendor;
 
 @Repository
 public interface VendorRepository extends JpaRepository<Vendor, UUID> {
@@ -15,6 +15,4 @@ public interface VendorRepository extends JpaRepository<Vendor, UUID> {
   Optional<Vendor> findByVendorIdentifier(UUID identifier);
 
   Boolean existsByVendorName(String vendorName);
-
-  Page<Vendor> findAllVendors(Pageable pageable);
 }
