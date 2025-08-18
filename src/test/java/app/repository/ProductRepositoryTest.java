67f6ff97780a41596241679f4c3d3b9bf0b1bb54
Xavier Callait <xaviercallait@gmail.com>
2025-08-18 12:25:26 -0700

    Starting adding basic tests

--- src/test/java/app/repository/ProductRepositoryTest.java
+++ src/test/java/app/repository/ProductRepositoryTest.java
@@ -1,19 +1,26 @@
 package app.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import app.model.Product;
-import app.model.Vendor;
-import app.model.category.Clothes;
-import app.model.category.types.ClothesTypes;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
+
+import app.model.Product;
+import app.model.Vendor;
+import app.model.category.Clothes;
+import app.model.category.Electronics;
+import app.model.category.types.ClothesTypes;
+import app.model.category.types.ElectronicsTypes;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -77,18 +84,60 @@ class ProductRepositoryTest {
     assertTrue(product.isEmpty());
   }
 
-  // @Test
-  // void shouldFindProductByVendorAndProductName() {
-  //   Product.Dimensions dimensions = new Product.Dimensions(10.0, 5.0, 2.0);
-  //   Vendor vendor = new Vendor("TestVendor");
-  //   Product testProduct = new Product(1L, 1000.00, "Name", "Location", 10.0, dimensions, vendor);
-  //   productRepository.save(testProduct);
-
-  //   Optional<Product> product =
-  // productRepository.findByVendorAndProductName(vendor.getVendorName(),
-  // testProduct.getProductName());
-  //   assertTrue(product.isPresent());
-  //   assertEquals(testProduct.getProductName(), product.get().getProductName());
-  //   assertEquals(testProduct.getProductIdentifier(), product.get().getProductIdentifier());
-  // }
+  @Test
+  void shouldFindProductByNameOfDifferentVendors() {
+    Product.Dimensions dimensions = new Product.Dimensions(10.0, 5.0, 2.0);
+    Vendor vendor1 = new Vendor("TestVendor1");
+    Electronics laptop1 =
+        new Electronics(
+            1L,
+            999.99,
+            "Laptop",
+            "Location",
+            10.0,
+            dimensions,
+            vendor1,
+            600.0,
+            5.0,
+            120.0,
+            ElectronicsTypes.ConnectivityTypes.BOTH,
+            ElectronicsTypes.PowerSourceTypes.RECHARGEABLE,
+            ElectronicsTypes.CurrentTypes.DC,
+            ElectronicsTypes.Types.LAPTOP);
+    Vendor vendor2 = new Vendor("TestVendor2");
+    Electronics laptop2 =
+        new Electronics(
+            1L,
+            999.99,
+            "Laptop",
+            "Location",
+            10.0,
+            dimensions,
+            vendor2,
+            600.0,
+            5.0,
+            120.0,
+            ElectronicsTypes.ConnectivityTypes.BOTH,
+            ElectronicsTypes.PowerSourceTypes.RECHARGEABLE,
+            ElectronicsTypes.CurrentTypes.DC,
+            ElectronicsTypes.Types.LAPTOP);
+    vendorRepository.save(vendor1);
+    vendorRepository.save(vendor2);
+    productRepository.save(laptop1);
+    productRepository.save(laptop2);
+    Page<Product> productPage = productRepository.findByProductName("Laptop", PageRequest.of(0, 3));
+    assertTrue(productPage.hasContent());
+    if (productPage.hasContent()) {
+      List<Product> products = productPage.getContent();
+      assertEquals(2, products.size());
+      assertEquals("Laptop", products.get(0).getProductName());
+      assertEquals("Laptop", products.get(1).getProductName());
+      for (Product p : products) {
+        if (p instanceof Electronics e) {
+          assertEquals(ElectronicsTypes.Types.LAPTOP, ElectronicsTypes.Types.fromDbValue(e.getType()));
+        }
+      }
+      assertNotEquals(products.get(0).getVendorID(), products.get(1).getVendorID());
+    }
+  }
 }
