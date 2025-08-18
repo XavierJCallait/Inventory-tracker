a65ae84717917b6f57ce029fce4374de47f4674a
Xavier Callait <xaviercallait@gmail.com>
2025-08-18 12:25:10 -0700

    Updated and added methods to product repository

--- src/main/java/app/repository/ProductRepository.java
+++ src/main/java/app/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package app.repository;
 
 import app.model.Product;
+import app.model.Vendor;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,18 +13,15 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, UUID> {
   Optional<Product> findByProductIdentifier(UUID identifier);
 
-  // Page<Product> findProductsByProductName(String productName, Pageable pageable);
+  Page<Product> findByProductName(String productName, Pageable pageable);
 
-  // Page<Product> findAllProducts(Pageable pageable);
+  Page<Product> findByVendor(Vendor vendor, Pageable pageable);
 
-  // Page<Product> findProductsByVendorName(String vendorName, Pageable pageable);
+  Optional<Product> findByVendorAndProductName(Vendor vendor, String productName);
 
-  // Page<Product> findProductsByVendorIdentifier(UUID vendorIdentifier, Pageable pageable);
+  Page<Product> findAllByPriceBetween(Double minPrice, Double maxPrice, Pageable pageable);
 
-  // Optional<Product> findByVendorAndProductName(String vendorName, String productName);
+  Page<Product> findAllByDiscountGreaterThan(Double discount, Pageable pageable);
 
-  // Optional<Product> findByVendorIdentifierAndProductName(UUID vendorIdentifier, String
-  // productName);
-
-  // Boolean existsByProductName(String productName);
+  Boolean existsByProductName(String productName);
 }
