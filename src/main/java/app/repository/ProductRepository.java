441f911d0deff196d775821f5d738303dbe1af61
Xavier Callait <xaviercallait@gmail.com>
2025-08-23 23:27:05 -0700

    Updated category to type

--- src/main/java/app/repository/ProductRepository.java
+++ src/main/java/app/repository/ProductRepository.java
@@ -1,15 +1,13 @@
 package app.repository;
 
+import app.model.Product;
+import app.model.Vendor;
 import java.util.Optional;
 import java.util.UUID;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import app.model.Product;
-import app.model.Vendor;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
